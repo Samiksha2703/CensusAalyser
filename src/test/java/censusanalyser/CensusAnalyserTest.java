4b8ce61d5bd434393574043c172c5f34366ed69c
Samiksha <shende.samiksha@rediffmail.com>
2021-04-02 21:08:38 +0530

    [Samiksha].Add unit test for wrong header.

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -9,7 +9,8 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_TYPE_FILE_PATH = "./src/test/resources/IndiaStateCensusData.txt";
-    private static final String INDIA_CENSUS_DATA_WRONG_DELIMETER_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusDataWrongDelimeter.csv";
+    private static final String INDIA_CENSUS_DATA_WRONG_DELI_METER_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusDataWrongDelimeter.csv";
+    private static final String WRONG_HEADER_PATH = "./src/test/resources/IndiaStateCensusDataWrongHeader.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -53,9 +54,21 @@ public class CensusAnalyserTest {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CensusAnalyserException.class);
-            censusAnalyser.checkDelimeter(INDIA_CENSUS_DATA_WRONG_DELIMETER_CSV_FILE_PATH);
+            censusAnalyser.checkDelimeter(INDIA_CENSUS_DATA_WRONG_DELI_METER_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELI_METER_DATA_FILE, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusDataWithWrongHeader_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.checkHeader(WRONG_HEADER_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_HEADER, e.type);
         }
     }
 }
