6cd083b0c44625400369b650f0930f722176aea5
Samiksha <shende.samiksha@rediffmail.com>
2021-04-02 20:47:34 +0530

    [Samiksha].Add unit test for wrong delimeter file type.

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -9,6 +9,7 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_TYPE_FILE_PATH = "./src/test/resources/IndiaStateCensusData.txt";
+    private static final String INDIA_CENSUS_DATA_WRONG_DELIMETER_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusDataWrongDelimeter.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -43,6 +44,18 @@ public class CensusAnalyserTest {
             censusAnalyser.loadIndiaCensusData(WRONG_TYPE_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_FILE_TYPE, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusDataWrongDeliMeterCSVFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.checkDelimeter(INDIA_CENSUS_DATA_WRONG_DELIMETER_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELI_METER_DATA_FILE, e.type);
         }
     }
 }
